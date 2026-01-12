dc2ea4197984d147c85ef4ba22129ea2c4c5ec31
SAfroz12 <affrozali9342@gmail.com>
2026-01-12 16:52:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+
+    const resetButton = document.getElementById("Reset");
+
+    changeButton.addEventListener("click", () => {
+      const blockId = document.getElementById("block_id").value;
+      const color = document.getElementById("colour_id").value;
+
+    
+      const allBlocks = document.querySelectorAll(".grid-item");
+      allBlocks.forEach(block => {
+        block.style.backgroundColor = "transparent";
+      });
+
+    
+      const selectedBlock = document.getElementById(blockId);
+      if (selectedBlock) {
+        selectedBlock.style.backgroundColor = color;
+      }
+    });
+
+    resetButton.addEventListener("click", () => {
+      const allBlocks = document.querySelectorAll(".grid-item");
+      allBlocks.forEach(block => {
+        block.style.backgroundColor = "transparent";
+      });
+    });required.
